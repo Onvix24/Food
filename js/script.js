4d2a2efa3b694792983bed14c087443128099a02
Onvix24 <maksdark2004@gmail.com>
2023-04-08 12:47:36 +0300

    add coments

--- js/script.js
+++ js/script.js
@@ -1,35 +1,46 @@
 window.addEventListener("DOMContentLoaded", function () {
   // Tabs
 
-  let tabs = document.querySelectorAll(".tabheader__item"),
-    tabsContent = document.querySelectorAll(".tabcontent"),
-    tabsParent = document.querySelector(".tabheader__items");
+  // Отримуємо всі елементи з класом ".tabheader__item"
+  let tabs = document.querySelectorAll(".tabheader__item");
+  // Отримуємо всі елементи з класом ".tabcontent"
+  let tabsContent = document.querySelectorAll(".tabcontent");
+  // Отримуємо батьківський елемент, щоб додати обробник подій
+  let tabsParent = document.querySelector(".tabheader__items");
 
+  // Функція для приховування вмісту всіх табів
   function hideTabContent() {
     tabsContent.forEach((item) => {
-      item.classList.add("hide");
-      item.classList.remove("show", "fade");
+      item.classList.add("hide"); // Додаємо клас "hide"
+      item.classList.remove("show", "fade"); // Видаляємо класи "show" та "fade"
     });
 
     tabs.forEach((item) => {
-      item.classList.remove("tabheader__item_active");
+      item.classList.remove("tabheader__item_active"); // Видаляємо клас "tabheader__item_active"
     });
   }
 
+  // Функція для відображення вмісту табу з індексом "i"
   function showTabContent(i = 0) {
-    tabsContent[i].classList.add("show", "fade");
-    tabsContent[i].classList.remove("hide");
-    tabs[i].classList.add("tabheader__item_active");
+    tabsContent[i].classList.add("show", "fade"); // Додаємо класи "show" та "fade"
+    tabsContent[i].classList.remove("hide"); // Видаляємо клас "hide"
+    tabs[i].classList.add("tabheader__item_active"); // Додаємо клас "tabheader__item_active"
   }
 
+  // Приховуємо вміст всіх табів
   hideTabContent();
+  // Відображуємо вміст першого табу
   showTabContent();
 
+  // Додаємо обробник подій "click" до батьківського елемента "tabsParent"
   tabsParent.addEventListener("click", function (event) {
-    const target = event.target;
+    const target = event.target; // Отримуємо цільовий елемент, на який клікнули
     if (target && target.classList.contains("tabheader__item")) {
+      // Перевіряємо, чи цільовий елемент має клас ".tabheader__item"
       tabs.forEach((item, i) => {
+        // Проходимо по всім елементам в "tabs"
         if (target == item) {
+          // Якщо збігається, то викликаємо функції "hideTabContent()" та "showTabContent(i)"
           hideTabContent();
           showTabContent(i);
         }
@@ -39,24 +50,32 @@ window.addEventListener("DOMContentLoaded", function () {
 
   // Timer
 
-  const deadline = "2023-04-03";
+  // Задаємо кінцевий термін в форматі рядка
+  const deadline = "2023-09-13";
 
+  // Функція для обчислення часу, який залишився до кінцевого терміну
   function getTimeRemaining(endtime) {
+    // Ініціалізуємо змінні, які будуть містити відповідні значення часу
     let days, seconds, minutes, hours;
+
+    // Обчислюємо час, який залишився до кінцевого терміну у мілісекундах
     const t = Date.parse(endtime) - Date.parse(new Date());
 
+    // Якщо залишилось менше 1 секунди, то усі значення часу будуть 0
     if (t <= 0) {
       days = 0;
       hours = 0;
       minutes = 0;
       seconds = 0;
     } else {
-      (days = Math.floor(t / (1000 * 60 * 60 * 24))),
-        (seconds = Math.floor((t / 1000) % 60)),
-        (minutes = Math.floor((t / 1000 / 60) % 60)),
-        (hours = Math.floor((t / (1000 * 60 * 60)) % 24));
+      // Інакше обчислюємо кількість днів, годин, хвилин та секунд
+      days = Math.floor(t / (1000 * 60 * 60 * 24));
+      seconds = Math.floor((t / 1000) % 60);
+      minutes = Math.floor((t / 1000 / 60) % 60);
+      hours = Math.floor((t / (1000 * 60 * 60)) % 24);
     }
 
+    // Повертаємо об'єкт зі значеннями часу
     return {
       total: t,
       days: days,
@@ -66,7 +85,7 @@ window.addEventListener("DOMContentLoaded", function () {
     };
   }
 
-  //Для моментального оновлення таймера на сторінці
+  // Функція, яка додає "0" перед значенням часу, якщо воно однозначне
   function getZero(num) {
     if (num >= 0 && num < 10) {
       return "0" + num;
@@ -74,76 +93,112 @@ window.addEventListener("DOMContentLoaded", function () {
       return num;
     }
   }
-
+  // Функція для відображення значень часу на сторінці
   function setClock(selector, endtime) {
+    // Отримуємо елементи таймера зі сторінки
     const timer = document.querySelector(selector),
       days = timer.querySelector("#days"),
       hours = timer.querySelector("#hours"),
       minutes = timer.querySelector("#minutes"),
-      seconds = timer.querySelector("#seconds"),
-      timeInterval = setInterval(updateClock, 1000);
+      seconds = timer.querySelector("#seconds");
 
+    // Встановлюємо інтервал для оновлення значень таймера кожну секунду
+    const timeInterval = setInterval(updateClock, 1000);
+
+    // Оновлюємо значення таймера
     updateClock();
 
     function updateClock() {
+      // Отримуємо значення часу залишку від функції getTimeRemaining()
       const t = getTimeRemaining(endtime);
+
+      // Відображаємо значення днів, годин, хвилин та секунд на сторінці
       days.innerHTML = getZero(t.days);
       hours.innerHTML = getZero(t.hours);
       minutes.innerHTML = getZero(t.minutes);
       seconds.innerHTML = getZero(t.seconds);
 
+      // Якщо час вичерпано, то зупиняємо інтервал
       if (t.total <= 0) {
         clearInterval(timeInterval);
       }
     }
   }
 
+  // Встановлюємо таймер на сторінку з використанням заданих параметрів
   setClock(".timer", deadline);
 
   // Modal
 
-  const modalTrigger = document.querySelectorAll("[data-modal]"),
-    modal = document.querySelector(".modal"),
-    modalCloseBtn = document.querySelector("[data-close]");
+  // Отримуємо всі кнопки, які мають атрибут data-modal
+  const modalTrigger = document.querySelectorAll("[data-modal]");
 
+  // Отримуємо модальне вікно та кнопку для закриття
+  const modal = document.querySelector(".modal");
+  const modalCloseBtn = document.querySelector("[data-close]");
+
+  // Функція, яка відкриває модальне вікно
   function openModal() {
+    // Додаємо клас "show" та видаляємо клас "hide" у модальному вікні, щоб показати його на сторінці
     modal.classList.add("show");
     modal.classList.remove("hide");
+
+    // Забороняємо прокручування сторінки, поки відображається модальне вікно
     document.body.style.overflow = "hidden";
+
+    // Очищуємо таймер, який автоматично відкриває модальне вікно через 5 секунд
     clearInterval(modalTimeId);
   }
+
+  // Додаємо обробник події "click" на всі кнопки з атрибутом data-modal, щоб відкривати модальне вікно при кліку
   modalTrigger.forEach((btn) => {
     btn.addEventListener("click", openModal);
   });
 
+  // Функція, яка закриває модальне вікно
   function closeModal() {
+    // Додаємо клас "hide" та видаляємо клас "show" у модальному вікні, щоб приховати його на сторінці
     modal.classList.add("hide");
     modal.classList.remove("show");
+
+    // Дозволяємо прокручування сторінки після закриття модального вікна
     document.body.style.overflow = "";
   }
+
+  // Додаємо обробник події "click" на модальне вікно, щоб закривати його при кліку поза вікном
   modal.addEventListener("click", (e) => {
     if (e.target === modal) {
       closeModal();
     }
   });
+
+  // Додаємо обробник події "click" на кнопку закриття модального вікна, щоб закрити його при кліку на кнопку
   modalCloseBtn.addEventListener("click", closeModal);
 
+  // Додаємо обробник події "keydown" на документ, щоб закривати модальне вікно при натисканні на клавішу Esc
   document.addEventListener("keydown", (e) => {
     if (e.code === "Escape" && modal.classList.contains("show")) {
       closeModal();
     }
   });
 
+  // Задаємо затримку появи модального вікна
   const modalTimeId = setTimeout(openModal, 5000);
 
+  // Функція, яка відкриває модальне вікно при скроллі до кінця сторінки
   function showModalByScroll() {
-      if (window.pageYOffset + document.documentElement.clientHeight >=
-        document.documentElement.scrollHeight -1) {
-          openModal();   
-          window.removeEventListener('scroll', showModalByScroll);
-      }
-  } 
+    if (
+      window.pageYOffset + document.documentElement.clientHeight >=
+      document.documentElement.scrollHeight - 1
+    ) {
+      openModal();
+      window.removeEventListener("scroll", showModalByScroll);
+    }
+  }
+  // Додаємо обробник події scroll до вікна браузера
+  window.addEventListener("scroll", showModalByScroll);
 
-  window.addEventListener('scroll', showModalByScroll)
 
+
+  
 });
