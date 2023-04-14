3d60418cb594c03ac42548ac8d0e20a9636be430
Onvix24 <maksdark2004@gmail.com>
2023-04-14 23:13:41 +0300

    fn postData is created through Fetch

--- js/script.js
+++ js/script.js
@@ -258,144 +258,108 @@ window.addEventListener("DOMContentLoaded", function () {
     }
   }
 
-  new MenuCard(
-    "img/tabs/vegy.jpg",
-    "vegy",
-    'Меню "Фитнес"',
-    'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
-    9,
-    ".menu .container",
-    "menu__item"
-  ).render();
+  const getResourse = async (url) => {
+    const res = await fetch(url);
 
-  new MenuCard(
-    "img/tabs/post.jpg",
-    "post",
-    'Меню "Постное"',
-    "Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.",
-    14,
-    ".menu .container",
-    "menu__item"
-  ).render();
+    if (!res.ok) {
+      throw new Error(`Could not fetch ${url}, status: ${error.status}`);
+    }
+    return await res.json();
+  };
 
-  new MenuCard(
-    "img/tabs/elite.jpg",
-    "elite",
-    "Меню “Премиум”",
-    "В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!",
-    21,
-    ".menu .container",
-    "menu__item"
-  ).render();
+  getResourse("http://localhost:3000/menu").then((data) => {
+    data.forEach(({ img, altimg, title, descr, price }) => {
+      new MenuCard(
+        img,
+        altimg,
+        title,
+        descr,
+        price,
+        ".menu .container"
+      ).render();
+    });
+  });
 
-<<<<<<< HEAD
-  // Forms
+  // getResource('http://localhost:3000/menu')
+  //     .then(data => createCard(data));
 
-  const forms = document.querySelectorAll("form");
-  const message = {
-    loading: "img/form/spinner.svg",
-=======
+  // function createCard(data) {
+  //     data.forEach(({img, altimg, title, descr, price}) => {
+  //         const element = document.createElement('div');
+
+  //         element.classList.add("menu__item");
+
+  //         element.innerHTML = `
+  //             <img src=${img} alt=${altimg}>
+  //             <h3 class="menu__item-subtitle">${title}</h3>
+  //             <div class="menu__item-descr">${descr}</div>
+  //             <div class="menu__item-divider"></div>
+  //             <div class="menu__item-price">
+  //                 <div class="menu__item-cost">Цена:</div>
+  //                 <div class="menu__item-total"><span>${price}</span> грн/день</div>
+  //             </div>
+  //         `;
+  //         document.querySelector(".menu .container").append(element);
+  //     });
+  // }
+
   // Вибираємо всі форми на сторінці
+
   const forms = document.querySelectorAll("form");
   // Створюємо об'єкт з повідомленнями, що будуть виводитися під час відправки форми
   const message = {
     loading: "Загрузка...",
->>>>>>> b9a28446534a5dddb344436fb488c46d152b3d21
     success: "Спасибо! Скоро мы с вами свяжемся",
     failure: "Что-то пошло не так...",
   };
 
-<<<<<<< HEAD
-=======
   // Додаємо обробник події на кожну форму на сторінці
->>>>>>> b9a28446534a5dddb344436fb488c46d152b3d21
   forms.forEach((item) => {
-    postData(item);
+    bindPostData(item);
   });
 
-  function postData(form) {
-<<<<<<< HEAD
+  const postData = async (url, data) => {
+    const res = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: data,
+    });
+
+    return await res.json();
+  };
+
+  function bindPostData(form) {
     form.addEventListener("submit", (e) => {
       e.preventDefault();
 
       let statusMessage = document.createElement("img");
       statusMessage.src = message.loading;
       statusMessage.style.cssText = `
-              display: block;
-              margin: 0 auto;
-          `;
+            display: block;
+            margin: 0 auto;
+        `;
       form.insertAdjacentElement("afterend", statusMessage);
 
-      const request = new XMLHttpRequest();
-      request.open("POST", "server.php");
-      request.setRequestHeader(
-        "Content-type",
-        "application/json; charset=utf-8"
-      );
       const formData = new FormData(form);
 
-=======
-    // Обробляємо подію "submit" на формі
-    form.addEventListener("submit", (e) => {
-      // Відміна стандартної поведінки браузера (надсилання форми)
-      e.preventDefault();
-      // Створюємо блок повідомлення про стан відправки форми
-      let statusMessage = document.createElement("div");
-      statusMessage.classList.add("status");
-      statusMessage.textContent = message.loading;
-      form.appendChild(statusMessage);
+      const json = JSON.stringify(Object.fromEntries(formData.entries()));
 
-      // Створюємо об'єкт запиту
-      const request = new XMLHttpRequest();
-      request.open("POST", "server.php"); // Вказуємо метод та URL сервера, який оброблятиме запит
-      request.setRequestHeader(
-        "Content-type",
-        "application/json; charset=utf-8"
-      ); // Встановлюємо заголовок запиту
-      const formData = new FormData(form); // Отримуємо дані з форми
-
-      // Створюємо об'єкт FormData з даних форми та перетворюємо його в об'єкт JavaScript
->>>>>>> b9a28446534a5dddb344436fb488c46d152b3d21
-      const object = {};
-      formData.forEach(function (value, key) {
-        object[key] = value;
-      });
-<<<<<<< HEAD
-      const json = JSON.stringify(object);
-
-      request.send(json);
-
-      request.addEventListener("load", () => {
-        if (request.status === 200) {
-          console.log(request.response);
+      postData("http://localhost:3000/requests", json)
+        .then((data) => {
+          console.log(data);
           showThanksModal(message.success);
           statusMessage.remove();
-          form.reset();
-        } else {
+        })
+        .catch(() => {
           showThanksModal(message.failure);
-=======
-      const json = JSON.stringify(object); // Перетворюємо об'єкт JavaScript в формат JSON
-
-      // Надсилаємо JSON-дані на сервер за допомогою запиту
-      request.send(json);
-
-      // Обробляємо подію завантаження запиту
-      request.addEventListener("load", () => {
-        if (request.status === 200) {
-          console.log(request.response);
-          statusMessage.textContent = message.success;
-          form.reset(); // Очищуємо форму
-          setTimeout(() => {
-            statusMessage.remove(); // Видаляємо повідомлення про стан відправки форми
-          }, 2000);
-        } else {
-          statusMessage.textContent = message.failure;
->>>>>>> b9a28446534a5dddb344436fb488c46d152b3d21
-        }
-      });
+        })
+        .finally(() => {
+          form.reset();
+        });
     });
   }
-<<<<<<< HEAD
 
   function showThanksModal(message) {
     const prevModalDialog = document.querySelector(".modal__dialog");
@@ -419,65 +383,4 @@ window.addEventListener("DOMContentLoaded", function () {
       closeModal();
     }, 4000);
   }
-=======
->>>>>>> b9a28446534a5dddb344436fb488c46d152b3d21
 });
-
-// Вибираємо всі форми на сторінці
-const forms = document.querySelectorAll("form");
-// Створюємо об'єкт з повідомленнями, що будуть виводитися під час відправки форми
-const message = {
-  loading: "Загрузка...",
-  success: "Спасибо! Скоро мы с вами свяжемся",
-  failure: "Что-то пошло не так...",
-};
-
-// Додаємо обробник події на кожну форму на сторінці
-forms.forEach((item) => {
-  postData(item);
-});
-
-function postData(form) {
-  // Обробляємо подію "submit" на формі
-  form.addEventListener("submit", (e) => {
-    // Відміна стандартної поведінки браузера (надсилання форми)
-    e.preventDefault();
-    // Створюємо блок повідомлення про стан відправки форми
-    let statusMessage = document.createElement("div");
-    statusMessage.classList.add("status");
-    statusMessage.textContent = message.loading;
-    form.appendChild(statusMessage);
-
-    // Створюємо об'єкт запиту
-    const request = new XMLHttpRequest();
-    request.open("POST", "server.php"); // Вказуємо метод та URL сервера, який оброблятиме запит
-    request.setRequestHeader("Content-type", "application/json; charset=utf-8"); // Встановлюємо заголовок запиту
-    const formData = new FormData(form); // Отримуємо дані з форми
-
-    // Створюємо об'єкт FormData з даних форми та перетворюємо його в об'єкт JavaScript
-    const object = {};
-    formData.forEach(function (value, key) {
-      object[key] = value;
-    });
-    const json = JSON.stringify(object); // Перетворюємо об'єкт JavaScript в формат JSON
-
-    // Надсилаємо JSON-дані на сервер за допомогою запиту
-    request.send(json);
-
-    // Обробляємо подію завантаження запиту
-    request.addEventListener("load", () => {
-      if (request.status === 200) {
-        console.log(request.response);
-        statusMessage.textContent = message.success;
-        form.reset(); // Очищуємо форму
-        setTimeout(() => {
-          statusMessage.remove(); // Видаляємо повідомлення про стан відправки форми
-        }, 2000);
-      }
-      else {
-        statusMessage.textContent = message.failure;
-      }
-    });
-  });
-}
-    
